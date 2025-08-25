6ac7b70951612d2e9506eb01c77b1711b834a6fa
kiaram12 <k_rojas@Kiaramrojas.>
2025-08-25 13:11:28 -0600

    first commit

---
+++ guess_age.py
@@ -0,0 +1,19 @@
+import random 
+
+print("Hi! I am going to try to guess your age.You think it is impossible for me. Let's see.")
+name = input("What is your name?")
+
+age = random.randint(15,30) 
+answer = input(f"Are you {age} years old? (y/n): ")
+
+while True:
+    age = random.randint(15,30)
+    answer = input(f"Are you {age} years old? : ")
+    if answer == 'y':
+        print(f"{name} is {age} years old!. I knew it")
+        break
+    else:
+        print("Rats.")
+        break 
+
+
